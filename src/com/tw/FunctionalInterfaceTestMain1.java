f5a9ca91cc179566307f9fccd6b2d1965b7ae252
6643644 <q543165@gmail.com>
2018-09-28 17:34:19 +0800

    new commit

---
+++ src/com/tw/FunctionalInterfaceTestMain1.java
@@ -0,0 +1,19 @@
+package com.tw;
+/**
+ * <p>
+ * JAVA8 的 Lambda 學習(1)
+ * </p>
+ * 
+ * @author miles
+ * @version 1.0, 2018/09/28
+ * @see
+ * @since
+ */
+public class FunctionalInterfaceTestMain1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
